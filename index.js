1dfa9f91a1a3b0d0cc9a10c2e083997828658368
Sam Waters <samwatersw15@gmail.com>
2019-04-15 07:40:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ function happyHolidays() {
   return 'Happy Holidays!'
 }
 function happyHolidaysTo(name) {
-  'Happy holidays, ${name}!'
+ return 'Happy holidays, ${name}!'
+}
+function happyHolidayTo(holiday,name) {
+  return 'Happy ${holiday}, ${name}!'
+}
+function holidayCountdown(holiday,days) {
+  return 'It\s ${days} days until ${holiday}!'
 }
